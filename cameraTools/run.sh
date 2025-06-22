f18c1605922778cbb6aa8493a56e4ca15365ee19
StuffzEZ <158745985+StuffzEZ@users.noreply.github.com>
2025-06-22 10:05:14 +0100

    Update run.sh

--- cameraTools/run.sh
+++ cameraTools/run.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 echo "HACombiner CameraTools Core Addon Installed And Loaded."
+echo "PLEASE NOTE: You are using the OLD REPO. Please go to https://hacombiner.github.io/cameratools To Upgrade."
 tail -f /dev/null
